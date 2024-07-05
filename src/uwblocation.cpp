ee4f9fbef742c26a063f98372e432953532e0763
kevinchen <kevingoes@126.com>
2024-07-05 15:37:45 +0800

    调整了小车位置的话题订阅

--- src/uwblocation.cpp
+++ src/uwblocation.cpp
@@ -5,26 +5,35 @@ unsigned char receive_buf[3000] = {0};
 int result = 0;
 bool isAutoposition = false;
 
-void receive_deal_func(serial::Serial& sp) {
+void receive_deal_func(serial::Serial &sp)
+{
 	// 初始化距离
 	int range[8] = {-1};
 
 	//  'mc' tag to anchor range bias corrected ranges – used for tag location
-	if ((receive_buf[0] == 'm') && (receive_buf[1] == 'c')) {
-		if (AutopositionMode == 1) {
-			if (!isAutoposition) {
+	if ((receive_buf[0] == 'm') && (receive_buf[1] == 'c'))
+	{
+		if (AutopositionMode == 1)
+		{
+			if (!isAutoposition)
+			{
 				ROS_ERROR_STREAM(
-				    "Using Autoposition Mode, but haven't autoposition yet!");
-				try {
+					"Using Autoposition Mode, but haven't autoposition yet!");
+				try
+				{
 					sp.write(order_start);
 					std::cout << "order_start sent successfully!\n";
-				} catch (const std::exception& e) {
+				}
+				catch (const std::exception &e)
+				{
 					std::cerr << "Failed to send order_start: " << e.what()
-					          << "\n";
+							  << "\n";
 				}
 				return;
 			}
-		} else if (AutopositionMode == 0) {
+		}
+		else if (AutopositionMode == 0)
+		{
 			// 手动基站标定，若基站位置修改则在下面更改标定坐标
 			// A0 uint:m
 			anchorArray(0, 0) = 0.0;
@@ -64,28 +73,33 @@ void receive_deal_func(serial::Serial& sp) {
 		int aid, tid, lnum, seq, mask;
 		int rangetime;
 		char role;
-		int data_len = strlen((char*)receive_buf);
+		int data_len = strlen((char *)receive_buf);
 
-		if (data_len == 106) {
-			int n = sscanf((char*)receive_buf,
-			               "mc %x %x %x %x %x %x %x %x %x %x %x %x %c%d:%d",
-			               &mask, &range[0], &range[1], &range[2], &range[3],
-			               &range[4], &range[5], &range[6], &range[7], &lnum,
-			               &seq, &rangetime, &role, &tid, &aid);
+		if (data_len == 106)
+		{
+			int n = sscanf((char *)receive_buf,
+						   "mc %x %x %x %x %x %x %x %x %x %x %x %x %c%d:%d",
+						   &mask, &range[0], &range[1], &range[2], &range[3],
+						   &range[4], &range[5], &range[6], &range[7], &lnum,
+						   &seq, &rangetime, &role, &tid, &aid);
 			printf("mask=0x%02x\nrange[0]=%d(mm)\nrange[1]=%d(mm)\nrange[2]=%d("
-			       "mm)\nrange[3]=%d(mm)\nrange[4]=%d(mm)\nrange[5]=%d(mm)"
-			       "\nrange[6]=%d(mm)\nrange[7]=%d(mm)\r\n",
-			       mask, range[0], range[1], range[2], range[3], range[4],
-			       range[5], range[6], range[7]);
-		} else if (data_len == 70) {
+				   "mm)\nrange[3]=%d(mm)\nrange[4]=%d(mm)\nrange[5]=%d(mm)"
+				   "\nrange[6]=%d(mm)\nrange[7]=%d(mm)\r\n",
+				   mask, range[0], range[1], range[2], range[3], range[4],
+				   range[5], range[6], range[7]);
+		}
+		else if (data_len == 70)
+		{
 			int n =
-			    sscanf((char*)receive_buf, "mc %x %x %x %x %x %x %x %x %c%d:%d",
-			           &mask, &range[0], &range[1], &range[2], &range[3], &lnum,
-			           &seq, &rangetime, &role, &tid, &aid);
+				sscanf((char *)receive_buf, "mc %x %x %x %x %x %x %x %x %c%d:%d",
+					   &mask, &range[0], &range[1], &range[2], &range[3], &lnum,
+					   &seq, &rangetime, &role, &tid, &aid);
 			printf("mask=0x%02x\nrange[0]=%d(mm)\nrange[1]=%d(mm)\nrange[2]=%d("
-			       "mm)\nrange[3]=%d(mm)\r\n",
-			       mask, range[0], range[1], range[2], range[3]);
-		} else {
+				   "mm)\nrange[3]=%d(mm)\r\n",
+				   mask, range[0], range[1], range[2], range[3]);
+		}
+		else
+		{
 			return;
 		}
 
@@ -100,29 +114,34 @@ void receive_deal_func(serial::Serial& sp) {
 	}
 	// 'ma' anchor to anchor range bias corrected ranges – used for anchor
 	// auto-positioning
-	else if ((receive_buf[0] == 'm') && (receive_buf[1] == 'a')) {
+	else if ((receive_buf[0] == 'm') && (receive_buf[1] == 'a'))
+	{
 		int aid, tid, lnum, seq, mask;
 		int rangetime;
 		char role;
 		int rx_power;
-		int data_len = strlen((char*)receive_buf);
+		int data_len = strlen((char *)receive_buf);
 		bool isSuccess = false;
 
-		if ((data_len == 106)) {
-			int n = sscanf((char*)receive_buf,
-			               "ma %x %x %x %x %x %x %x %x %x %x %x %x %c%d:%d %x",
-			               &mask, &range[0], &range[1], &range[2], &range[3],
-			               &range[4], &range[5], &range[6], &range[7], &lnum,
-			               &seq, &rangetime, &role, &tid, &aid, &rx_power);
+		if ((data_len == 106))
+		{
+			int n = sscanf((char *)receive_buf,
+						   "ma %x %x %x %x %x %x %x %x %x %x %x %x %c%d:%d %x",
+						   &mask, &range[0], &range[1], &range[2], &range[3],
+						   &range[4], &range[5], &range[6], &range[7], &lnum,
+						   &seq, &rangetime, &role, &tid, &aid, &rx_power);
 			printf("mask=0x%02x\nrange[0]=%d(mm)\nrange[1]=%d(mm)\nrange[2]=%d("
-			       "mm)\nrange[3]=%d(mm)\nrole=%c:%d\r\n",
-			       mask, range[0], range[1], range[2], range[3], role, aid);
+				   "mm)\nrange[3]=%d(mm)\nrole=%c:%d\r\n",
+				   mask, range[0], range[1], range[2], range[3], role, aid);
 
-			if (n == EOF) {
+			if (n == EOF)
+			{
 				ROS_ERROR_STREAM("data Error !!!");
 				return;
 			}
-		} else {
+		}
+		else
+		{
 			ROS_ERROR_STREAM("data is not 106!");
 			return;
 		}
@@ -136,58 +155,70 @@ void receive_deal_func(serial::Serial& sp) {
 
 		double dist = (anchorArray - anchorArray_last).rowwise().norm().sum();
 
-		if (isSuccess && (dist < 0.05 * 4)) {
+		if (isSuccess && (dist < 0.05 * 4))
+		{
 			// 成功标定且上下两次标定的坐标偏差不超过0.05m
 			isAutoposition = true;
 			// 发送串口指令'$ancrangestop\r\n'，关闭基站自标定功能，开始标签定位
 			ROS_WARN_STREAM("Self-calibration Succeed!");
 			printf("A0:(%f, %f, %f),A1:(%f, %f, %f),A2:(%f, %f, %f),A3:(%f, "
-			       "%f, %f)\n",
-			       anchorArray(0, 0), anchorArray(0, 1), anchorArray(0, 2),
-			       anchorArray(1, 0), anchorArray(1, 1), anchorArray(1, 2),
-			       anchorArray(2, 0), anchorArray(2, 1), anchorArray(2, 2),
-			       anchorArray(3, 0), anchorArray(3, 1), anchorArray(3, 2));
+				   "%f, %f)\n",
+				   anchorArray(0, 0), anchorArray(0, 1), anchorArray(0, 2),
+				   anchorArray(1, 0), anchorArray(1, 1), anchorArray(1, 2),
+				   anchorArray(2, 0), anchorArray(2, 1), anchorArray(2, 2),
+				   anchorArray(3, 0), anchorArray(3, 1), anchorArray(3, 2));
 
-			try {
+			try
+			{
 				sp.write(order_stop);
 				std::cout << "order_stop sent successfully!\n";
-			} catch (const std::exception& e) {
+			}
+			catch (const std::exception &e)
+			{
 				std::cerr << "Failed to send order_stop: " << e.what() << "\n";
 			}
 
 			return;
 		}
 		return;
-	} else {
+	}
+	else
+	{
 		puts("No suitable message!\n");
 		return;
 	}
 }
 
-void CtrlSerDataDeal(serial::Serial& sp) {
+void CtrlSerDataDeal(serial::Serial &sp)
+{
 	unsigned char middata = 0;
 	static unsigned char dataTmp[MAX_DATA_NUM] = {0};
 
-	while (BufCtrlPosit_r != BufCtrlPosit_w) {
+	while (BufCtrlPosit_r != BufCtrlPosit_w)
+	{
 		middata = BufDataFromCtrl[BufCtrlPosit_r];
 		BufCtrlPosit_r =
-		    (BufCtrlPosit_r == MAX_DATA_NUM - 1) ? 0 : (BufCtrlPosit_r + 1);
+			(BufCtrlPosit_r == MAX_DATA_NUM - 1) ? 0 : (BufCtrlPosit_r + 1);
 
 		if (((middata == DataHead) || (middata == DataHead2)) &&
-		    (rcvsign == 0)) // 收到头
+			(rcvsign == 0)) // 收到头
 		{
-			rcvsign = 1;                     // 开始了一个数据帧
+			rcvsign = 1;					 // 开始了一个数据帧
 			dataTmp[DataRecord++] = middata; // 数据帧接收中
-		} else if ((middata != DataTail) && (rcvsign == 1)) {
-			dataTmp[DataRecord++] = middata; // 数据帧接收中
-		} else if ((middata == DataTail) && (rcvsign == 1)) // 收到尾
+		}
+		else if ((middata != DataTail) && (rcvsign == 1))
 		{
-			if (DataRecord != 1) {
+			dataTmp[DataRecord++] = middata; // 数据帧接收中
+		}
+		else if ((middata == DataTail) && (rcvsign == 1)) // 收到尾
+		{
+			if (DataRecord != 1)
+			{
 				rcvsign = 0;
 				dataTmp[DataRecord++] = middata;
 				dataTmp[DataRecord] = '\0';
 
-				strncpy((char*)receive_buf, (char*)dataTmp, DataRecord);
+				strncpy((char *)receive_buf, (char *)dataTmp, DataRecord);
 				printf("receive_buf = %slen = %d\n", receive_buf, DataRecord);
 				receive_deal_func(sp); /*调用处理函数*/
 				bzero(receive_buf, sizeof(receive_buf));
@@ -198,35 +229,40 @@ void CtrlSerDataDeal(serial::Serial& sp) {
 	}
 }
 
-void anchor1_pos_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+void anchor1_pos_callback(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
 	geometry_msgs::PoseStamped current_vrpn = *msg;
 	anchorArray(0, 0) = current_vrpn.pose.position.x;
 	anchorArray(0, 1) = current_vrpn.pose.position.y;
 	anchorArray(0, 2) = current_vrpn.pose.position.z + 0.15;
 }
 
-void anchor2_pos_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+void anchor2_pos_callback(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
 	geometry_msgs::PoseStamped current_vrpn = *msg;
 	anchorArray(1, 0) = current_vrpn.pose.position.x;
 	anchorArray(1, 1) = current_vrpn.pose.position.y;
 	anchorArray(1, 2) = current_vrpn.pose.position.z + 0.15;
 }
 
-void anchor3_pos_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+void anchor3_pos_callback(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
 	geometry_msgs::PoseStamped current_vrpn = *msg;
 	anchorArray(2, 0) = current_vrpn.pose.position.x;
 	anchorArray(2, 1) = current_vrpn.pose.position.y;
 	anchorArray(2, 2) = current_vrpn.pose.position.z + 0.15;
 }
 
-void anchor4_pos_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+void anchor4_pos_callback(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
 	geometry_msgs::PoseStamped current_vrpn = *msg;
 	anchorArray(3, 0) = current_vrpn.pose.position.x;
 	anchorArray(3, 1) = current_vrpn.pose.position.y;
 	anchorArray(3, 2) = current_vrpn.pose.position.z + 0.15;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	setlocale(LC_ALL, "");
 	std_msgs::String msg;
 	std_msgs::String msg_mc;
@@ -238,7 +274,7 @@ int main(int argc, char** argv) {
 	ros::NodeHandle nh;
 	ros::NodeHandle nh1;
 	ros::Publisher uwb_publisher =
-	    nh.advertise<uwb_location::uwb>("/uwb/data", 100); // 发布tag的定位信息
+		nh.advertise<uwb_location::uwb>("/uwb/data", 100); // 发布tag的定位信息
 
 	// 创建一个serial类
 	serial::Serial sp;
@@ -252,7 +288,7 @@ int main(int argc, char** argv) {
 	sp.setTimeout(to);
 
 	// Load configs.
-	nh.param("AutopositionMode", AutopositionMode, 0);
+	nh.param("AutopositionMode", AutopositionMode, 2);
 	nh.param("TagpositionMode", TagpositionMode, 1);
 
 	std::string anchor1_pos_topic;
@@ -264,43 +300,57 @@ int main(int argc, char** argv) {
 	nh.param("anchor3_pos", anchor3_pos_topic);
 	nh.param("anchor4_pos", anchor4_pos_topic);
 
-	try {
+	try
+	{
 		// 打开串口
 		sp.open();
-	} catch (serial::IOException& e) {
+	}
+	catch (serial::IOException &e)
+	{
 		ROS_ERROR_STREAM("Unable to open port.");
 		return -1;
 	}
 
 	// 判断串口是否打开成功
-	if (sp.isOpen()) {
+	if (sp.isOpen())
+	{
 		ROS_INFO_STREAM("/dev/usb_uwb is opened.");
-	} else {
+	}
+	else
+	{
 		return -1;
 	}
 
 	// ros::Rate loop_rate(11);
 
-	if (AutopositionMode == 1) {
+	if (AutopositionMode == 1)
+	{
 		std::cout << "AutopositionMode == 1!" << std::endl;
-		try {
+		try
+		{
 			sp.write(order_start);
 			std::cout << "order_start sent successfully!\n";
-		} catch (const std::exception& e) {
+		}
+		catch (const std::exception &e)
+		{
 			std::cerr << "Failed to send data: " << e.what() << "\n";
 		}
-	} else if (AutopositionMode == 2) {
+	}
+	else if (AutopositionMode == 2)
+	{
 		anchor1_pos_sub =
-		    nh.subscribe(anchor1_pos_topic, 10, anchor1_pos_callback);
+			nh.subscribe("/vrpn_client_node/tb0/pose", 10, anchor1_pos_callback);
 		anchor2_pos_sub =
-		    nh.subscribe(anchor2_pos_topic, 10, anchor2_pos_callback);
+			nh.subscribe("/vrpn_client_node/tb1/pose", 10, anchor2_pos_callback);
 		anchor3_pos_sub =
-		    nh.subscribe(anchor3_pos_topic, 10, anchor3_pos_callback);
+			nh.subscribe("/vrpn_client_node/tb2/pose", 10, anchor3_pos_callback);
 		anchor4_pos_sub =
-		    nh.subscribe(anchor4_pos_topic, 10, anchor4_pos_callback);
+			nh.subscribe("/vrpn_client_node/tb3/pose", 10, anchor4_pos_callback);
 		std::cout << "AutopositionMode == 2!" << std::endl;
 		std::cout << "Subsribed to 4 anchors' positon topic!" << std::endl;
-	} else if (AutopositionMode == 0) {
+	}
+	else if (AutopositionMode == 0)
+	{
 		std::cout << "AutopositionMode == 0!" << std::endl;
 		std::cout << "Using fixed anchor position!" << std::endl;
 	}
@@ -308,29 +358,33 @@ int main(int argc, char** argv) {
 	// 发布uwb话题
 	uwb_location::uwb uwb_data;
 
-	while (ros::ok()) {
+	while (ros::ok())
+	{
 		// 获取缓冲区内的字节数
 		size_t len = sp.available();
 
-		if (len > 0) {
+		if (len > 0)
+		{
 			unsigned char usart_buf[1024] = {0};
 			sp.read(usart_buf, len);
 
-			unsigned char* pbuf;
+			unsigned char *pbuf;
 			unsigned char buf[2014] = {0};
 
-			pbuf = (unsigned char*)usart_buf;
+			pbuf = (unsigned char *)usart_buf;
 			memcpy(&buf[0], pbuf, len);
 
 			int reallength = len;
 			int i;
-			if (reallength != 0) {
-				for (i = 0; i < reallength; i++) {
+			if (reallength != 0)
+			{
+				for (i = 0; i < reallength; i++)
+				{
 					BufDataFromCtrl[BufCtrlPosit_w] = buf[i];
 
 					BufCtrlPosit_w = (BufCtrlPosit_w == (MAX_DATA_NUM - 1))
-					                     ? 0
-					                     : (1 + BufCtrlPosit_w);
+										 ? 0
+										 : (1 + BufCtrlPosit_w);
 				}
 			}
 			CtrlSerDataDeal(sp);
